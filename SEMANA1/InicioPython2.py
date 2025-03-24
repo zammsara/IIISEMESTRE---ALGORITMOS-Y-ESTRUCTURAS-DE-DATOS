64bcb42704c095648ea047c724f513b8a6e9659b
zammsara <saztaylor@uamv.edu.ni>
2025-03-24 07:53:00 -0600

    SEMANA1

---
+++ SEMANA1/InicioPython2.py
@@ -0,0 +1,7 @@
+print("Hola mucho gusto")
+nombre = input("Ingresa tu nombre: ")
+edad = int(input("Introduce tu edad: "))
+print(nombre, edad, sep='')
+
+precio = float(input("Introduce el precio de la computadora: "))
+print("La computadora cuesta:", precio, seps='', end = '*')
